08db0f51dc3754538ce3acd48d38b1a2e7c71e2f
fouzia07724 <59837518+fouzia07724@users.noreply.github.com>
2020-01-15 20:34:03 +0000

    In Progress

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,41 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+
+// 1. Find the valuable things
+
+const secretNumber = 6;
+
+const squares = document.querySelectorAll('.square') as NodeListOf<HTMLDivElement>;
+
+let currentSquare = 1;
+squares.forEach(sq => {
+    if (currentSquare === secretNumber) {
+        sq.dataset.secret = 'true';
+    }
+    currentSquare++;
+    sq.addEventListener('click', handleClick);
+});
+
+function handleClick() {
+    const that = this as HTMLDivElement;
+    const isWinner = that.dataset.secret === 'true';
+    if (isWinner) {
+        that.classList.add('winner');
+    } else {
+        that.classList.add('loser');
+
+    }
+    that.removeEventListener('click', handleClick);
+}
+
+// once you win , all the other guessess be marked as losers
+
+// Maybe make a play again button that resets everything
+// If you win, tell them how awesome they are
+// If they loose (you picked everyone but the Winner!) . they should be told they loose.
+
+
+
+
+
 
